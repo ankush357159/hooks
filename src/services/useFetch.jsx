d80c60ee20cf279c3aa4181b6425fb8d663106e0
ankush357159 <ankush357159@gmail.com>
2024-06-01 21:55:25 +0530

    useFetch custom hook

---
+++ src/services/useFetch.jsx
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const url = process.env.REACT_APP_URL;
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(url);
+        setData(response.data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+  return { data, loading, error };
+};
+
+export default useFetch;
