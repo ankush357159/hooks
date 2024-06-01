ebff25b93beed9a9ddf9b184eb9e16be057bd97b
ankush357159 <ankush357159@gmail.com>
2024-06-01 21:54:54 +0530

    External data component

---
+++ src/components/ExternalDataComponent.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import fetchDataFromExternalSystem from "../services/fetchDataFromExternalSystem";
+import "./ExternalDataComponent.css";
+
+const ExternalDataComponent = () => {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetchDataFromExternalSystem().then((response) => setData(response));
+  }, []);
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      {data ? (
+        <div style={{ width: "70%" }}>
+          <h2 style={{ textAlign: "center" }}>Data from external system</h2>
+          <table style={{ width: "100%" }}>
+            <thead>
+              <tr>
+                <th>Sr.No.</th>
+                <th>Name</th>
+                <th>Username</th>
+                <th>Email</th>
+                <th>City</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((user, index) => (
+                <tr key={user.id}>
+                  <td>{index + 1}</td>
+                  <td>{user.name}</td>
+                  <td>{user.username}</td>
+                  <td>{user.email}</td>
+                  <td>{user.address.city}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </div>
+  );
+};
+
+export default ExternalDataComponent;
